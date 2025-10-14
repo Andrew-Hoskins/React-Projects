a4b91a094ae00d2af19585cf1824f8921fd976c8
Andrew-Hoskins <AndyHoskins87+git@gmail.com>
2025-10-14 16:19:23 +0100

    created a text-expander component

---
+++ 3.steps/src/app-v1.jsx
@@ -0,0 +1,71 @@
+import { useState } from "react";
+
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setisOpen] = useState(true);
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+  }
+  function handleNext() {
+    if (step < 3) setStep((s) => s + 1);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setisOpen((isopen) => !isopen)}>
+        &times;
+      </button>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+          <div className="buttons">
+            <Button
+              bgColor={"#7950f2"}
+              textcolor={"#fff"}
+              onClick={handlePrevious}
+            >
+              <span>👈</span> Previous
+            </Button>
+
+            <Button bgColor={"#7950f2"} textcolor={"#fff"} onClick={handleNext}>
+              <span>👉</span> Next
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  );
+}
+
+function Button({ textcolor, bgColor, onClick, children }) {
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textcolor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
--- 8.text-expander/src/main.jsx
+++ 8.text-expander/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./app-v1.jsx";
+
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
